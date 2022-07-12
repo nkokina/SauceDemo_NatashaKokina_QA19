8fdf3559e1196d248239edaa857da0cd8e0f0eee
nkokina <mae9077@gmail.com>
2022-07-12 22:42:48 +0300

    I made corrections to ItemDetalisPage - removed unnecessary elements and methods, renamed methods.

--- src/main/java/pages/ItemDetailsPage.java
+++ src/main/java/pages/ItemDetailsPage.java
@@ -9,20 +9,14 @@ public class ItemDetailsPage extends HomePage {
     private final By itemName = By.cssSelector(".inventory_details_name");
     private final By itemDescription = By.cssSelector(".inventory_details_desc");
     private final By itemPrice = By.cssSelector(".inventory_details_price");
-    private final By itemNameInBasket = By.cssSelector(".inventory_item_name");
-    private final By itemDescriptionInBasket = By.cssSelector(".inventory_item_desc");
-    private final By itemPriceInBasket = By.cssSelector(".inventory_item_price");
-    private final By shoppingCart = By.cssSelector(".shopping_cart_link");
+
     private final By removeButton =  By.cssSelector("button[id^=remove-sauce-labs-backpack]");
 
     public ItemDetailsPage(WebDriver driver) {
         super(driver);
     }
-    public boolean isRemoveButton (){
+    public boolean isRemoveButtonDisplayed (){
         return driver.findElement(removeButton).isDisplayed();
-    }
-    public void clickingOnTheShoppingCart() {
-        driver.findElement(shoppingCart).click();
     }
 
     public void clickAddToCartButton() {
@@ -40,16 +34,6 @@ public class ItemDetailsPage extends HomePage {
     public String getItemPrice() {
         return driver.findElement(itemPrice).getText();
     }
-    public String getItemNameInBasket () {
-        return driver.findElement(itemNameInBasket).getText();
-    }
 
-    public String getItemDescriptionInBasket () {
-        return driver.findElement(itemDescriptionInBasket).getText();
-    }
-
-    public String getItemPriceInBasket () {
-        return driver.findElement(itemPriceInBasket).getText();
-    }
 
 }
