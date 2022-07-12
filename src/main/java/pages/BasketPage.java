490a9dc4ac51314784040342ac37a9b505319bee
nkokina <mae9077@gmail.com>
2022-07-12 22:48:00 +0300

    Added BasketPage.java

---
+++ src/main/java/pages/BasketPage.java
@@ -0,0 +1,25 @@
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class BasketPage extends BasePage{
+    private final By itemNameInBasket = By.cssSelector(".inventory_item_name");
+    private final By itemDescriptionInBasket = By.cssSelector(".inventory_item_desc");
+    private final By itemPriceInBasket = By.cssSelector(".inventory_item_price");
+
+    public BasketPage(WebDriver driver) {
+        super(driver);
+    }
+    public String getItemNameInBasket () {
+        return driver.findElement(itemNameInBasket).getText();
+    }
+
+    public String getItemDescriptionInBasket () {
+        return driver.findElement(itemDescriptionInBasket).getText();
+    }
+
+    public String getItemPriceInBasket () {
+        return driver.findElement(itemPriceInBasket).getText();
+    }
+}
