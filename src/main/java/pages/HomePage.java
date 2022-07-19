f240ba9906ce3924f3de209cd96f983efa785620
nkokina <mae9077@gmail.com>
2022-07-19 23:41:38 +0300

    Made some minor tweaks

--- src/main/java/pages/HomePage.java
+++ src/main/java/pages/HomePage.java
@@ -5,6 +5,11 @@ import org.openqa.selenium.WebDriver;
 
 public abstract class HomePage extends BasePage {
 
+    protected By twitterInFooter = By.cssSelector(".social_twitter");
+
+    protected By facebookInFooter = By.cssSelector(".social_facebook");
+    protected By linkedinInFooter = By.cssSelector(".social_linkedin");
+    protected By footerCopyText = By.cssSelector(".footer_copy");
     protected By shoppingCart = By.cssSelector(".shopping_cart_link");
     protected By menuPage = By.cssSelector(".bm-burger-button");
     protected By allItemsMenu = By.id("inventory_sidebar_link");
@@ -14,6 +19,22 @@ public abstract class HomePage extends BasePage {
 
     public HomePage(WebDriver driver) {
         super(driver);
+    }
+
+    public String getFooterCopyText() {
+        return driver.findElement(footerCopyText).getText();
+    }
+
+    public void clickTwitterInFooter() {
+        driver.findElement(twitterInFooter).click();
+    }
+
+    public void clickFacebookInFooter() {
+        driver.findElement(facebookInFooter).click();
+    }
+
+    public void clickLinkedinInFooter() {
+        driver.findElement(linkedinInFooter).click();
     }
 
     public void clickMenuPage() {
